d4c236c99d1a6d829e8b693c6c4ecec054d17825
Kyle Decot <kyle@joinroot.com>
2018-02-05 12:00:07 -0500

    Adds ability to extract the subfile designator

--- lib/aamva/decoder.rb
+++ lib/aamva/decoder.rb
@@ -1,21 +1,42 @@
 module AAMVA
   class Decoder
-    def initialize(spec, barcode)
-      @spec = spec
+    def initialize(barcode)
+      @spec = YAML.load_file(File.expand_path("../../../lib/aamva/data/info/2016.yml", __FILE__))
       @barcode = barcode
+      @regexp = /\A(?<sub_fille_type>DL|ID)(?<offset>\d{4})(?<length>\d{4})Z\w(\d{4})(\d{4})(\w{2})(?<data_elements>.+)/m
+      @header_regexp = /@\n\rANSI (?<issuer_identification_number>\d{6})(?<aamva_version_number>\d{2})(?<jurisdiction_version_number>\d{2})(?<number_of_entries>\d{2})/
+      @subfile_designator_regexp = /(?<subfile_type>DL|ID)(?<offset>\d{4})(?<length>\d{4}).+(\k<subfile_type>)/
+
     end
 
-    def method_missing(method_name, *args)
-      data_element = method_name.to_s.upcase
+    def header
+      header_match.to_s
+    end
 
-      extract(data_element, @barcode)
+    def subfile_designator
+      @barcode.match(@subfile_designator_regexp)&.[](0)
+    end
+
+    def aamva_version_number
+      header_match&.[]("aamva_version_number")
+    end
+
+    def issuer_identification_number
+      header_match&.[]("issuer_identification_number")
+    end
+
+    def jurisdiction_version_number
+      header_match&.[]("jurisdiction_version_number")
+    end
+
+    def number_of_entries
+      header_match&.[]("number_of_entries")
     end
 
     private
 
-    def extract(key, string)
-      matched = string.match(/^#{key}(?<value>.+)/)
-      matched&.[]("value")
+    def header_match
+      @barcode.match(@header_regexp)
     end
   end
 end
--- spec/aamva/decoder_spec.rb
+++ spec/aamva/decoder_spec.rb
@@ -1,27 +1,53 @@
 require 'spec_helper'
 
 RSpec.describe AAMVA::Decoder do
-  let(:barcode) { "@\n\rANSI 636023080102DL00410279ZO03200024DLDBA09142019\nDCSDECOT\nDACKYLE\nDADBRANDON\nDBD10032015\nDBB09141986\nDBC1\nDAYHAZ\nDAU070 IN\nDAG1437 CHESAPEAKE AVE\nDAICOLUMBUS\nDAJOH\nDAK432122152  \nDAQSS430403\nDCF2509UN6813300000\nDCGUSA\nDDEN\nDDFN\nDDGN\nDAZBRO\nDCIUS,OHIO\nDCJNONE\nDCUNONE\nDCE4\nDDAM\nDDB12042013\nDAW170\nDCAD\nDCBA\nDCDNONE\rZOZOAN\nZOBN\nZOE09142019\r" }
-  let(:spec) { YAML.load_file(File.expand_path("../../../lib/aamva/data/info/2016.yml", __FILE__))}
+  describe "when valid" do
+    let(:barcode) { "@\n\rANSI 636023080102DL00410279ZO03200024DLDBA09142019\nDCSDECOT\nDACKYLE\nDADBRANDON\nDBD10032015\nDBB09141986\nDBC1\nDAYHAZ\nDAU070 IN\nDAG1437 CHESAPEAKE AVE\nDAICOLUMBUS\nDAJOH\nDAK432122152  \nDAQSS430403\nDCF2509UN6813300000\nDCGUSA\nDDEN\nDDFN\nDDGN\nDAZBRO\nDCIUS,OHIO\nDCJNONE\nDCUNONE\nDCE4\nDDAM\nDDB12042013\nDAW170\nDCAD\nDCBA\nDCDNONE\rZOZOAN\nZOBN\nZOE09142019\r" }
 
+    subject { described_class.new(barcode) }
 
-  describe ".day" do
-    it { expect(described_class.new(spec, barcode).day).to eq('HAZ') }
+    describe "#header" do
+      it { expect(subject.header).to eq("@\n\rANSI 636023080102") }
+    end
+
+    describe "#subfile_designator" do
+      it { expect(subject.subfile_designator).to eq("DL00410279ZO03200024DL") }
+    end
+
+    describe "#aamva_version_number" do
+      it { expect(subject.aamva_version_number).to eq("08") }
+    end
+
+    describe "#issuer_identification_number" do
+      it { expect(subject.issuer_identification_number).to eq("636023") }
+    end
+
+    describe "#jurisdiction_version_number" do
+      it { expect(subject.jurisdiction_version_number).to eq("01") }
+    end
+
+    describe "#number_of_entries" do
+      it { expect(subject.number_of_entries).to eq("02") }
+    end
+
+    # describe "#day" do
+    #   it { expect(subject.day).to eq('HAZ') }
+    # end
+    #
+    # describe "#dcs" do
+    #   it { expect(subject.dcs).to eq('DECOT') }
+    # end
+    #
+    # describe "#dac" do
+    #   it { expect(subject.dac).to eq('KYLE') }
+    # end
+    #
+    # describe "#dad" do
+    #   it { expect(subject.dad).to eq('BRANDON') }
+    # end
+    #
+    # describe "#dba" do
+    #   it { expect(subject.dba).to eq('09142019') }
+    # end
   end
-
-  describe ".dcs" do
-    it { expect(described_class.new(spec, barcode).dcs).to eq('DECOT') }
-  end
-
-  describe ".dac" do
-    it { expect(described_class.new(spec, barcode).dac).to eq('KYLE') }
-  end
-
-  describe ".dad" do
-    it { expect(described_class.new(spec, barcode).dad).to eq('BRANDON') }
-  end
-
-  # describe ".dba" do
-  #   it { expect(described_class.new(spec, barcode).dba).to eq('09142019') }
-  # end
 end
