0a00d311cbd0053f9b306922c616a017255ba10d
Kyle Decot <kyle@joinroot.com>
2018-02-14 15:29:02 -0500

    Adds specs for Standard instantiation

--- spec/aamva/standard_spec.rb
+++ spec/aamva/standard_spec.rb
@@ -1,4 +1,21 @@
 require "spec_helper"
 
 RSpec.describe AAMVA::Standard do
-end 
+  describe ".initialize" do
+    describe "when standard is supported" do
+      it "does not raise" do
+        expect do
+          described_class.new("2016")
+        end.to_not raise_error(AAMVA::Standard::UnsupportedStandard)
+      end
+    end
+
+    describe "when standard is not supported" do
+      it "raises" do
+        expect do
+          described_class.new("FOOBAR")
+        end.to raise_error(AAMVA::Standard::UnsupportedStandard)
+      end
+    end
+  end
+end
